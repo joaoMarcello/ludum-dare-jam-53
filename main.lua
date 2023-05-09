dd6c2e74da4cce5a9e1b9e3a074f6b6deafaf2ee
Joao Moreira <joaomarcello.mm@gmail.com>
2023-05-09 11:23:50 -0300

    estavel

--- main.lua
+++ main.lua
@@ -135,7 +135,7 @@ end
 
 local km = 0
 function love.update(dt)
-    km = collectgarbage("count") / 1024.0
+    -- km = collectgarbage("count") / 1024.0
     Pack:update(dt)
     SceneManager.scene:update(dt)
 end
@@ -143,13 +143,13 @@ end
 function love.draw()
     SceneManager.scene:draw()
 
-    lgx.setColor(0, 0, 0, 0.7)
-    lgx.rectangle("fill", 0, 0, 80, 120)
-    lgx.setColor(1, 1, 0, 1)
-    lgx.print(string.format("Memory:\n\t%.2f Mb", km), 5, 10)
-    lgx.print("FPS: " .. tostring(love.timer.getFPS()), 5, 50)
-    local maj, min, rev, code = love.getVersion()
-    lgx.print(string.format("Version:\n\t%d.%d.%d", maj, min, rev), 5, 75)
+    -- lgx.setColor(0, 0, 0, 0.7)
+    -- lgx.rectangle("fill", 0, 0, 80, 120)
+    -- lgx.setColor(1, 1, 0, 1)
+    -- lgx.print(string.format("Memory:\n\t%.2f Mb", km), 5, 10)
+    -- lgx.print("FPS: " .. tostring(love.timer.getFPS()), 5, 50)
+    -- local maj, min, rev, code = love.getVersion()
+    -- lgx.print(string.format("Version:\n\t%d.%d.%d", maj, min, rev), 5, 75)
 
     -- local stats = love.graphics.getStats()
     -- local font = _G.JM_Font
